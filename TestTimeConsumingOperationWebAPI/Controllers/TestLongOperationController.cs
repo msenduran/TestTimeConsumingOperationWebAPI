4c6f7d3ecd8ceb600850e50ce272589213669507
msenduran <mericsndrn@gmail.com>
2025-12-14 20:03:09 +0300

    Proje dosyası ekle.

---
+++ TestTimeConsumingOperationWebAPI/Controllers/TestLongOperationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestTimeConsumingOperationWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestLongOperationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(5000); //Representing a long operation
+
+            return Content("Web API Long Operation Completed");
+
+        }
+
+    }
+}
---
+++ TestTimeConsumingOperationWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestTimeConsumingOperationWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
